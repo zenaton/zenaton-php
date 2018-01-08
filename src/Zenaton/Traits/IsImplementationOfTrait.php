0f48f0fc2ec5c86dd29f1b9bc5863df2de6f0cdb
Gilles Barbier <gilles@thefamily.co>
2018-01-08 17:36:12 +0100

    update Version implentation

--- src/Zenaton/Traits/IsImplementationOfTrait.php
+++ src/Zenaton/Traits/IsImplementationOfTrait.php
@@ -7,7 +7,7 @@ trait IsImplementationOfTrait
     protected function isImplementationOf($name, $class)
     {
         if (is_string($name)) {
-            $implements = class_implements($name);
+            $implements = @class_implements($name);
             if (is_array($implements)) {
                 return isset($implements[$class]);
             }
--- src/Zenaton/Workflows/Version.php
+++ src/Zenaton/Workflows/Version.php
@@ -4,11 +4,15 @@ namespace Zenaton\Workflows;
 
 use ReflectionClass;
 use Zenaton\Interfaces\WorkflowInterface;
+use Zenaton\Interfaces\VersionInterface;
 use Zenaton\Traits\Zenatonable;
+use Zenaton\Traits\IsImplementationOfTrait;
+use Zenaton\Exceptions\ExternalZenatonException;
 
 abstract class Version implements WorkflowInterface
 {
     use Zenatonable;
+    use IsImplementationOfTrait;
 
     protected $args;
 
@@ -17,18 +21,51 @@ abstract class Version implements WorkflowInterface
         $this->args = func_get_args();
     }
 
+    // history of implementation
+    abstract public function versions();
+
     // useful for local implementation
     public function handle()
     {
         return ($this->getCurrentImplementation())->handle();
     }
 
-    // class name of current implementation
-    abstract protected function current();
-
-    // true current implementation
+    // current implementation
     public function getCurrentImplementation()
     {
         return (new ReflectionClass($this->current()))->newInstanceArgs($this->args);
+    }
+
+    // class name of current implementation
+    public function current()
+    {
+        return array_values(array_slice($this->_getVersions(), -1))[0];
+    }
+
+    // class name of initial implementation
+    public function initial()
+    {
+        return array_values(array_slice($this->_getVersions(), 0))[0];
+    }
+
+    protected function _getVersions()
+    {
+        $versions = $this->versions();
+
+        if (! is_array($versions)) {
+            throw new ExternalZenatonException("'versions' method must return an array");
+        }
+
+        if (count($versions) == 0) {
+            throw new ExternalZenatonException("'versions' method must return at least one element");
+        }
+
+        foreach ($versions as $key => $class) {
+            if (! $this->isImplementationOf($class, WorkflowInterface::class)) {
+                throw new ExternalZenatonException("Element returned by 'versions' method for key '" . $key . "' is not the name of a class implementing " . WorkflowInterface::class);
+            }
+        }
+
+        return $versions;
     }
 }
