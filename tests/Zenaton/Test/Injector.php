6f6874923f927296a96470010356767b89882434
Pierre-Yves Lebecq <pierre-yves@zenaton.com>
2018-09-12 12:15:35 +0200

    Add CircleCI build on PHP 5.6, 7.0, 7.1, 7.2

--- tests/Zenaton/Test/Injector.php
+++ tests/Zenaton/Test/Injector.php
@@ -7,10 +7,13 @@ trait Injector
     /**
      * Binds a given closure to an object and executes it.
      *
+     * The given closure will not receive any parameter.
+     *
      * @param object $object
      */
     protected function inject(\Closure $closure, $object)
     {
-        $closure->call($object);
+        $boundedClosure = $closure->bindTo($object, get_class($object));
+        $boundedClosure();
     }
 }
