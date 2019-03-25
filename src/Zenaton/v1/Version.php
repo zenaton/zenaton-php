3762443fe26907c57abcd4452c5ff7638ac3dfb6
Pierre-Yves Lebecq <pierre-yves@zenaton.com>
2019-03-25 15:58:46 +0100

    Prepare for release 0.3.0

--- src/Zenaton/v1/Version.php
+++ src/Zenaton/v1/Version.php
@@ -11,10 +11,10 @@ namespace Zenaton;
  */
 final class Version
 {
-    const FULL = '0.3.0-DEV';
+    const FULL = '0.3.0';
     const ID = 00300;
     const MAJOR = 0;
     const MINOR = 3;
     const PATCH = 0;
-    const EXTRA = 'DEV';
+    const EXTRA = '';
 }
