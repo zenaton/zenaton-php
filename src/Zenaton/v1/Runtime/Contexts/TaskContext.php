3d009e5e027c85e01c3bcaab83e11a00073c604d
Pierre-Yves Lebecq <pierre-yves@zenaton.com>
2019-08-19 14:46:17 +0200

    Rename attempt index to retry index

--- src/Zenaton/v1/Runtime/Contexts/TaskContext.php
+++ src/Zenaton/v1/Runtime/Contexts/TaskContext.php
@@ -3,26 +3,26 @@
 namespace Zenaton\Runtime\Contexts;
 
 /**
- * Represents the current runtime context of a class.
+ * Represents the current runtime context of a task.
  *
- * The information provided by the context can be useful to alter the behaviour of the task.
- * For example, you can use the attempt index to know if a task has been automatically retried or not and how many times,
+ * The information provided by the context can be useful to alter the behaviour of the task if required.
+ * For example, you can use the retry index to know if a task has been automatically retried or not and how many times,
  * and decide to do something when you did not expect the task to be retried more than X times.
- * You can also use the attempt number in the `onErrorRetryDelay` method of a task in order to implement complex
+ * You can also use the retry index in the `onErrorRetryDelay` method of a task in order to implement complex
  * retry strategies.
  */
 final class TaskContext
 {
     /**
-     * The current attempt index.
+     * The current retry index.
      *
      * When the task is executed for the first time, the value will be `1`.
-     * Every time the task is automatically retried, the value is increased.
+     * Every time the task is automatically retried, the value is increased by one.
      * When a manual retry is instructed, the value goes back to `1`.
      *
      * @var null|int
      */
-    public $attemptIndex;
+    public $retryIndex;
 
     /**
      * The task identifier.
@@ -33,7 +33,7 @@ final class TaskContext
 
     public function __construct(array $values = [])
     {
-        $this->attemptIndex = isset($values['attempt_index']) ? (int) $values['attempt_index'] : null;
+        $this->retryIndex = isset($values['retry_index']) ? (int) $values['retry_index'] : null;
         $this->id = isset($values['id']) ? (string) $values['id'] : null;
     }
 }
